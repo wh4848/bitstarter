f854ab462f7acac7e256b6f5145cc37edc634e9c
sujin choi <wh4848@naver.com>
2013-07-15 02:01:40 +0900

    Updated index.html, web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var buf = fs.readFileSync('index.html');
+  var data = buf.toString('utf-8', 0, buf.length);
+
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
